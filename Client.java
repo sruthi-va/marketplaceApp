bde6adb0cc0d663aeae333e92095a8a6202162bc
jenniferwang <jenniferwpdx2018@gmail.com>
2022-11-17 12:21:29 -0500

    username and password

--- Client.java
+++ Client.java
@@ -4,7 +4,9 @@ import java.net.*;
 public class Client {
     public static void main(String[] args) throws IOException {
         boolean run = true;
-        //ocket socket = new Socket("localhost", 4242);
+        //Socket socket = new Socket("localhost", 4242);
+        String userName = "";
+        String password = "";
         String userChoice[] = {"Customer", "Seller"};
         String choose = "";
         while (run) {
@@ -16,16 +18,39 @@ public class Client {
                     return;
                 }
 
+                do {
+                    userName = JOptionPane.showInputDialog(null, "Please enter username.",
+                            "Welcome", JOptionPane.QUESTION_MESSAGE);
+                } while (userName.isEmpty());
+                if (userName == null) {
+                    run = false;
+                } else {
+                    do {
+                        password = (JOptionPane.showInputDialog(null, "Please enter password.",
+                                "Welcome", JOptionPane.QUESTION_MESSAGE));
+
+                    } while (password.isEmpty());
+                }
+                if (password == null) {
+                    run = false;
+                }
+
                 choose = (String) JOptionPane.showInputDialog(null, "Are you a customer or seller?",
                         "Choice?", JOptionPane.QUESTION_MESSAGE,
                         null, userChoice, userChoice[0]);
                 if (choose == null) {
                     run = false;
                 }
+                if (choose.equals("Customer")) {
+
+                }
+                if (choose.equals("Seller")) {
+
+                }
 
 
             } catch (Exception e) {
-                JOptionPane.showMessageDialog(null, "", "Welcome",
+                JOptionPane.showMessageDialog(null, "Thanks for visiting bEtsy!", "Goodbye",
                         JOptionPane.ERROR_MESSAGE);
                 return;
             }
